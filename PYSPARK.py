551f1703dd261b5f507ce25b3706b2fb6289fe16
naufalhadir <naufal.hadi@ui.ac.id>
2017-12-05 22:51:02 +0700

    PYSPARK.py

---
+++ PYSPARK.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# In[4]:
+
+get_ipython().system('pip install pyspark')
+
+
+# In[ ]:
+
+set a = 10
+
+
+# In[ ]:
+
+a
+
+
+# In[6]:
+
+from pyspark import SparkContext,SparkConf
+sc = SparkContext()
+
+
+# In[7]:
+
+import os
+
+
+# In[8]:
+
+os.getcwd()
+
+
+# In[ ]:
+
+
+
